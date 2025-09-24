5a62c739ba6e6bdda4234be9493433509b343282
ljh <15001297+ljh133@user.noreply.gitee.com>
2025-09-24 15:48:21 +0800

    9.24

--- camera_to_lidar/output/final-extrinsic.py
+++ camera_to_lidar/output/final-extrinsic.py
@@ -1,65 +1,35 @@
-    import numpy as np
-    import json
+import numpy as np
+import json
 
-    def load_json(file_path):
-        """加载JSON文件并返回字典"""
-        with open(file_path, 'r') as file:
-            return json.load(file)
+def load_json(file_path):
+    """加载JSON文件并返回字典"""
+    with open(file_path, 'r') as file:
+        return json.load(file)
 
-    def get_matrices(file_data, input_key):
-        """根据指定的键获取相应的矩阵"""
-        matrix = np.array(file_data[input_key])
-        return matrix
+def get_matrices(file_data, input_key):
+    """根据指定的键获取相应的矩阵"""
+    matrix = np.array(file_data[input_key])
+    return matrix
 
-    def compute_frontfisheye2m(camera2lidar, lidar2m32, output_path, input_key, output_key):
-        if '-' in input_key:
-            input_data = camera2lidar
-        else:
-            input_data = lidar2m32
+def compute_sensor2mcenter(camera2lidar, lidar2m32, output_path, input_key, output_key):
+    #工装值
+    center_matrix = np.array([[1, 0, 0, 1.2], [0, 1, 0, 0], [0, 0, 1, 1.76], [0, 0, 0, 1]])
 
-        # 使用新函数获取矩阵
+    if '-' in input_key:
+        input_data = camera2lidar
+    elif input_key == "zhulidar":
+        input_data = np.eye(4)
+    else:
+        input_data = lidar2m32
+
+    # 使用新函数获取矩阵
+    if input_key == "zhulidar":
+        matrix = input_data
+    else:
         matrix = get_matrices(input_data, input_key)
-    
-        center_matrix = np.array([[1, 0, 0, 1.2], [0, 1, 0, 0], [0, 0, 1, 1.76], [0, 0, 0, 1]])
-        # 计算frontfisheye2m矩阵
-        frontfisheye2m_matrix = np.dot(center_matrix, matrix)
 
-        # 读取现有数据
-        try:
-            with open(output_path, 'r') as outfile:
-                output_data = json.load(outfile)
-        except FileNotFoundError:
-            output_data = {}
-
-        # 更新数据
-        output_data[output_key] = frontfisheye2m_matrix.tolist()
-
-        # 将结果保存到输出文件中
-        with open(output_path, 'w') as outfile:
-            json.dump(output_data, outfile, indent=4)
-
-        print(f"{output_key} Matrix has been saved to {output_path}")
-
-    # 加载JSON文件
-    camera2m32 = load_json('camera2m32.json')
-    lidar2m32 = load_json('lidar2m32.json')
-
-    # 定义键和输出路径列表
-    camera_keys = ["front-fisheye2m32", "left-fisheye2m32", "right-fisheye2m32","front-pinhole2m32","back-pinhole2m32"]
-    lidar_keys = ["front","back"]
-    output_keys = ["front-fisheye", "left-fisheye", "right-fisheye","front-pinhole","back-pinhole","frontlidar","backlidar"]
-    output_path = "final-extrinsic.json"
-
-    # 清空输出文件
-    with open(output_path, 'w') as f:
-        json.dump({}, f)
-    # 分别调用函数
-    all_keys = camera_keys + lidar_keys
-    for input_key, output_key in zip(all_keys, output_keys):
-        compute_frontfisheye2m(camera2m32, lidar2m32, output_path, input_key, output_key)
-
-    # 添加主加光雷达到中心点的外参为单位阵
-    identity_matrix = np.eye(4)  # 4x4单位矩阵
+    # 计算sensor2mcenter矩阵
+    sensor2mcenter_matrix = np.dot(center_matrix, matrix)
 
     # 读取现有数据
     try:
@@ -68,11 +38,29 @@
     except FileNotFoundError:
         output_data = {}
 
-    # 添加单位矩阵到输出数据
-    output_data["lidar_to_center"] = identity_matrix.tolist()
+    # 更新数据
+    output_data[output_key] = sensor2mcenter_matrix.tolist()
 
-    # 保存更新后的数据到输出文件
+    # 将结果保存到输出文件中
     with open(output_path, 'w') as outfile:
         json.dump(output_data, outfile, indent=4)
 
-    print("lidar_to_center Matrix (Identity) has been saved to", output_path)
+    print(f"{output_key} Matrix has been saved to {output_path}")
+
+# 加载JSON文件
+camera2m32 = load_json('camera2m32.json')
+lidar2m32 = load_json('lidar2m32.json')
+
+# 定义键和输出路径列表
+camera_keys = ["front-fisheye2m32", "left-fisheye2m32", "right-fisheye2m32","front-pinhole2m32","back-pinhole2m32"]
+lidar_keys = ["front","back"]
+output_keys = ["front-fisheye", "left-fisheye", "right-fisheye","front-pinhole","back-pinhole","frontlidar","backlidar","zhulidar"]
+output_path = "final-extrinsic.json"
+
+# 清空输出文件
+with open(output_path, 'w') as f:
+    json.dump({}, f)
+# 分别调用函数
+all_keys = camera_keys + lidar_keys+ ["zhulidar"]
+for input_key, output_key in zip(all_keys, output_keys):
+    compute_sensor2mcenter(camera2m32, lidar2m32, output_path, input_key, output_key)
